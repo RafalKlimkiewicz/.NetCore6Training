e205a2ca3622e77e4b65cbb630ed9f3cc26adb5f
Rafal <klimek1008@gmail.com>
2024-03-21 20:13:09 +0100

    Initial Commit

---
+++ SimpleApp/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public decimal? Decimal { get; set; }
+
+    }
+}
